7071574e447ee517431af8b5ace582c78ba430c2
Mir Rawrs <mirrawrs@outlook.com>
2017-03-09 17:49:20 -0500

    Add constructors to ComponentDescriptionAttribute and ComponentNameAttribute.

--- Lotus.Dispatching/Attributes/ComponentDescriptionAttribute.cs
+++ Lotus.Dispatching/Attributes/ComponentDescriptionAttribute.cs
@@ -4,6 +4,11 @@ namespace Lotus.Dispatching.Attributes
 {
     public class ComponentDescriptionAttribute : Attribute
     {
+        public ComponentDescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
         public string Description { get; set; }
     }
 }
--- Lotus.Dispatching/Attributes/ComponentNameAttribute.cs
+++ Lotus.Dispatching/Attributes/ComponentNameAttribute.cs
@@ -4,6 +4,11 @@ namespace Lotus.Dispatching.Attributes
 {
     public class ComponentNameAttribute : Attribute
     {
+        public ComponentNameAttribute(string name)
+        {
+            Name = name;
+        }
+
         public string Name { get; set; }
     }
 }
